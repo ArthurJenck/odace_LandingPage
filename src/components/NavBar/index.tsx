04058b97887c9a317141bc8ab462a55ce69e8924
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-13 17:48:38 +0100

    feat: made navbar responsive

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,19 +1,35 @@
+import odaceLogo from "../../assets/odaceLogo.svg"
 import aiSearchIcon from "../../assets/aiSearchIcon.svg"
 import "./NavBar.scss"
 
 const NavBar = () => {
   return (
     <nav>
-      <div className="nav-left">
-        <a href="https://odaceplus.com/fr/a-propos">Plans</a>
-        <a href="https://odaceplus.com/fr/a-propos">À Propos</a>
-      </div>
-      <div className="nav-right">
+      <div className="nav-mobile">
+        <a href="https://odaceplus.com/fr/" className="logo">
+          <img src={odaceLogo} alt="logo odace+" />
+        </a>
         <a href="https://odaceplus.com/fr/search">
           <img src={aiSearchIcon} alt="Icone de loupe" />
         </a>
-        <a href="https://odaceplus.com/fr/signin">Connexion</a>
-        <a href="https://odaceplus.com/fr/signup">Inscription</a>
+        <div className="burger-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className="nav-desk">
+        <div className="nav-left">
+          <a href="https://odaceplus.com/fr/a-propos">Plans</a>
+          <a href="https://odaceplus.com/fr/a-propos">À Propos</a>
+        </div>
+        <div className="nav-right">
+          <a href="https://odaceplus.com/fr/search">
+            <img src={aiSearchIcon} alt="Icone de loupe" />
+          </a>
+          <a href="https://odaceplus.com/fr/signin">Connexion</a>
+          <a href="https://odaceplus.com/fr/signup">Inscription</a>
+        </div>
       </div>
     </nav>
   )
